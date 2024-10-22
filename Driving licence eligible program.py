3ce21338d3226b4d1067f5ae2ddb46f84e5d99f6
Sumit210106 <sumitnayak2006@gmail.com>
2024-10-22 21:21:03 +0530

    Update Driving licence eligible program.py
    I updated and added iteration conditional statement ....if someone enters inavalid age .... with message

--- Driving licence eligible program.py
+++ Driving licence eligible program.py
@@ -1,5 +1,7 @@
-a=int(input("Enter your age"))
-if(a>=18):
+age=int(input("Enter your age"))
+if(0>=age>=18):
   print("you are eligible for driving!!")
+elif 0>age:
+  print("Enter Valid age!!!")
 else:
   print("you are not eligible for driving!!")
